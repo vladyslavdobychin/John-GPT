aefa000fc0a3af1bfbd722762a37e5860d39933e
VladDobychin <vladyslav.dobychin@binary-studio.com>
2024-08-13 17:23:05 +0200

    REST-setup: + created a basic controller, model and migration

--- john-php/routes/api.php
+++ john-php/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NotesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\GPTApiController;
@@ -25,3 +26,4 @@ Route::get('/hello', function () {
 
 Route::get('/chat', [GPTApiController::class, 'createChat']);
 
+Route::resource('/home', NotesController::class);
