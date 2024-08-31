bb3ad2754106ec901ad52f87838827e55b864a56
VladDobychin <vladyslav.dobychin@binary-studio.com>
2024-08-31 19:25:06 +0200

    REST-setup: + added phpdoc for type hinting

--- john-php/app/Models/Note.php
+++ john-php/app/Models/Note.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static find($id)
+ * @method static create($data)
+ */
+
 class Note extends Model
 {
     use HasFactory;
