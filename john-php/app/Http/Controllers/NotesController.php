568a55cd3ad0e2bc85068da30e9a5abf90c99202
VladDobychin <vladyslav.dobychin@binary-studio.com>
2024-08-31 17:46:03 +0200

    REST-setup: - removed unnecessary methods

--- john-php/app/Http/Controllers/NotesController.php
+++ john-php/app/Http/Controllers/NotesController.php
@@ -30,25 +30,9 @@ class NotesController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
     {
         //
     }
