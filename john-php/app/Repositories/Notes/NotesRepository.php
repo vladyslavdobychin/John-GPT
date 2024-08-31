258d2dbcb616d66b1f4f1fb425b78a9817833831
VladDobychin <vladyslav.dobychin@binary-studio.com>
2024-08-31 16:50:43 +0200

    REST-setup: + added typing

--- john-php/app/Repositories/Notes/NotesRepository.php
+++ john-php/app/Repositories/Notes/NotesRepository.php
@@ -3,15 +3,16 @@
 namespace App\Repositories\Notes;
 
 use App\Models\Note;
+use Illuminate\Database\Eloquent\Collection;
 
 class NotesRepository implements NotesRepositoryInterface
 {
-    public function getAllNotes()
+    public function getAllNotes(): Collection
     {
         return Note::all();
     }
 
-    public function getNoteById($id)
+    public function getNoteById($id): Note
     {
         return Note::find($id);
     }
