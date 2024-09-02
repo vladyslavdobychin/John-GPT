0640171ca5c8182da3103e9647b594f16b9a821a
VladDobychin <vladyslav.dobychin@binary-studio.com>
2024-09-02 17:35:36 +0200

    REST-setup: + added soft delete

--- john-php/app/Models/Note.php
+++ john-php/app/Models/Note.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @method static find($id)
@@ -14,6 +15,7 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = ['title', 'content'];
 
